db49aaf36f2c37a34643285491a910c8986cd960
Razvan Constantin <razvanco13@gmail.com>
2025-12-14 22:38:01 +0100

    Added support for branch and parameters to cmakeGitLib.

--- packages/cpp/cmake.ts
+++ packages/cpp/cmake.ts
@@ -1,21 +1,63 @@
 import { existsSync, mkdirSync } from "node:fs";
 import { SourceDirContext } from "../core/context";
 import { build } from "../make/cmd";
-import { join } from 'node:path';
+import { join } from "node:path";
 
-export async function cmakeGitLib(ctx: SourceDirContext, name: string, gitUrl: string): Promise<string> {
+export async function cmakeGitLib(
+  ctx: SourceDirContext,
+  name: string,
+  params: {
+    gitUrl: string;
+    gitBranch?: string;
+    cmakeDefs?: any;
+    makeTargets?: string[];
+  },
+): Promise<string> {
   const cwd = ctx.outputPath(true);
   const dir = join(cwd, name);
   if (existsSync(dir)) {
-    await build(ctx, { dir }, { cmd: ['git', 'pull'], cwd: dir });
+    if (params.gitBranch) {
+      await build(
+        ctx,
+        { dir },
+        { cmd: ["git", "checkout", params.gitBranch], cwd: dir },
+      );
+    }
+    await build(ctx, { dir }, { cmd: ["git", "pull"], cwd: dir });
   } else {
-    await build(ctx, { dir }, { cmd: ['git', 'clone', gitUrl], cwd });
+    const cmd = ["git", "clone", params.gitUrl];
+    if (params.gitBranch) {
+      cmd.push(`--branch=${params.gitBranch}`);
+    }
+    await build(ctx, { dir }, { cmd, cwd });
   }
-  const buildDir = join(dir, 'build');
+  const buildDir = join(dir, "build");
   if (!existsSync(buildDir)) {
     mkdirSync(buildDir, { recursive: true });
   }
-  await build(ctx, { label: name }, { cmd: ['cmake', '..'], cwd: buildDir });
-  await build(ctx, { label: name }, { cmd: ['make', '-j'], cwd: buildDir });
+  await build(
+    ctx,
+    { label: `cmake configure ${name}` },
+    {
+      cmd: ["cmake", ".."].concat(cmakeDefsFlags(params.cmakeDefs)),
+      cwd: buildDir,
+    },
+  );
+  await build(
+    ctx,
+    { label: `make ${name}` },
+    { cmd: ["make", "-j"].concat(params.makeTargets || []), cwd: buildDir },
+  );
   return dir;
+}
+
+function cmakeDefsFlags(defs?: any): string[] {
+  if (!defs) {
+    return [];
+  }
+  const flags: string[] = [];
+  for (const def in defs) {
+    flags.push(`-D${def}=${defs[def]}`);
+  }
+  return flags;
 }
