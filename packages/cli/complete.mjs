aa1578043093c9de5838dcb4995d08e60e4720bb
Razvan Constantin <razvanco13@gmail.com>
2026-01-03 12:37:58 +0100

    Prevent override directory by a sub-directory expansion

--- packages/cli/complete.mjs
+++ packages/cli/complete.mjs
@@ -1,5 +1,5 @@
 import { existsSync, lstatSync, opendirSync } from "node:fs";
-import { basename, dirname, resolve } from "node:path";
+import { basename, dirname, resolve, join } from "node:path";
 import { stdout } from "node:process";
 import { getExportedFunctions } from "./exported_functions.mjs";
 import { resolveScript } from "./make.mjs";
@@ -13,24 +13,26 @@ function listDirs(dirPath) {
   try {
     const dir = opendirSync(resolvedPath);
     for (let subdir = dir.readSync(); subdir; subdir = dir.readSync()) {
-      if (subdir.name.startsWith('.') || subdir.name === 'node_modules') {
+      if (subdir.name.startsWith(".") || subdir.name === "node_modules") {
         continue;
       }
       if (subdir.isDirectory()) {
-        dirnames.push(subdir.name + '/');
+        dirnames.push(join(dirPath, subdir.name));
       }
     }
     dir.close();
   } catch (error) {
     // Ignore silently
   }
-  return dirnames
+  return dirnames;
 }
 
 function hasJavascriptExt(filename) {
-  return filename.endsWith('.js')
-    || filename.endsWith('.mjs')
-    || filename.endsWith('.cjs');
+  return (
+    filename.endsWith(".js") ||
+    filename.endsWith(".mjs") ||
+    filename.endsWith(".cjs")
+  );
 }
 
 function listScripts(dirPath) {
@@ -43,7 +45,7 @@ function listScripts(dirPath) {
     const dir = opendirSync(resolvedPath);
     for (let file = dir.readSync(); file; file = dir.readSync()) {
       if (file.isFile() && hasJavascriptExt(file.name)) {
-        scripts.push(file.name);
+        scripts.push(join(dirPath, file.name));
       }
     }
     dir.close();
@@ -54,11 +56,11 @@ function listScripts(dirPath) {
 }
 
 function completeDirs(target) {
-  if (target.endsWith('/')) {
+  if (existsSync(target) && lstatSync(target).isDirectory()) {
     return listDirs(target);
   }
   const targetName = basename(target);
-  if (targetName.indexOf(':') >= 0) {
+  if (targetName.indexOf(":") >= 0) {
     // No completion: already expanding methods
     return [];
   }
@@ -67,15 +69,15 @@ function completeDirs(target) {
     return [];
   }
   const dirs = listDirs(dirname(target));
-  return dirs.filter(dir => dir.startsWith(targetName));
+  return dirs.filter((dir) => dir.startsWith(target));
 }
 
 function completeScripts(target) {
-  if (target.endsWith('/') || existsSync(target) && lstatSync(target).isDirectory()) {
+  if (existsSync(target) && lstatSync(target).isDirectory()) {
     return listScripts(target);
   }
   const targetName = basename(target);
-  if (targetName.includes(':')) {
+  if (targetName.includes(":")) {
     // No completion: already expanding methods
     return [];
   }
@@ -84,15 +86,15 @@ function completeScripts(target) {
     return [];
   }
   const scripts = listScripts(dirname(target));
-  return scripts.filter(script => script.startsWith(targetName));
+  return scripts.filter((script) => script.startsWith(target));
 }
 
 function resolveTargetScript(target) {
-  if (target.includes(':')) {
-    target = target.split(':', 2)[0];
+  if (target.includes(":")) {
+    target = target.split(":", 2)[0];
   }
-  if (target === '') {
-    target = '.';
+  if (target === "") {
+    target = ".";
   }
   return resolveScript(target).script;
 }
@@ -102,29 +104,31 @@ function listScriptMethods(script) {
 }
 
 function completeFunctions(word) {
-  const lastColon = word.lastIndexOf(':');
-  const target = (lastColon === -1) ? word : word.substring(0, lastColon);
-  const prefix = (lastColon === -1) ? '' : word.substring(lastColon + 1);
+  const lastColon = word.lastIndexOf(":");
+  const target = lastColon === -1 ? word : word.substring(0, lastColon);
+  const prefix = lastColon === -1 ? "" : word.substring(lastColon + 1);
   const script = resolveTargetScript(target);
   if (!script) {
     return [];
   }
-  const methods = listScriptMethods(script).filter(m => m.startsWith(prefix));
-  return methods.map(method => `${target}:${method}`);
+  const methods = listScriptMethods(script).filter((m) => m.startsWith(prefix));
+  return methods.map((method) => `${target}:${method}`);
 }
 
 export async function complete(args) {
   const compCWord = Number.parseInt(args[0]);
   const compWords = args.slice(1);
-  const word = compWords[compCWord] || '';
+  const word = compWords[compCWord] || "";
 
   const completions = [];
   if (compCWord === 1) {
-    completions.push(...['config', 'make', 'watch'].filter(cmd => cmd.startsWith(word)));
+    completions.push(
+      ...["config", "make", "watch"].filter((cmd) => cmd.startsWith(word)),
+    );
   }
   completions.push(...completeDirs(word));
   completions.push(...completeScripts(word));
   completions.push(...completeFunctions(word));
 
-  stdout.write(completions.join(' '));
+  stdout.write(completions.join(" "));
 }
